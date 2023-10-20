bebe318ba986fc2145c6c042b208d122959fe070
Shivram Shrestha <SSHRESTHA@hubgroup.com>
2023-10-19 20:15:38 -0500

    form added

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,60 +1,105 @@
 <template>
   <form action="">
     <label>Email</label>
-    <input type="email" required v-model="email"/>
+    <input type="email" required v-model="email" />
 
     <label>Password: </label>
 
-        <input type="password" required v-model="password"/>
-  
+    <input type="password" required v-model="password" />
+
+    <label>Role : </label>
+    <select v-model="role">
+      <option value="developer">Web Developer</option>
+      <option value="designer">Web Designer</option>
+    </select>
+
+    <label> Skill: </label>
+    <input type="text" v-model="tempSkil" @keyup="addSkills" />
+
+    <div v-for="skill in skills" :key="skill" class="pill">
+      {{ skill }}
+    </div>
+
+    <div class="terms">
+      <input type="checkbox" v-model="terms" required />
+      <label id="changeFontSize"> Accept terms and condition </label>
+    </div>
   </form>
 
-  <p>Email : {{email }}</p>
+  <!-- <p>Email : {{ email }}</p>
 
-  <p>Password : {{password}}</p>
+  <p>Password : {{ password }}</p>
+
+  <p>Role is : {{ role }}</p>
+
+  <p>Terms : {{ terms }}</p> -->
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      role: "designer",
+      terms: true,
+      tempSkill: "",
+      skills: [],
+    };
+  },
 
-    data(){
-        return{
-            email: '',
-            password: ''
-        }
-    }
-}
+  methods: {
+    addSkills(e) {
+      console.log(e);
+      if (e.key === " " && this.tempSkill) {
+        this.skills.push(this.tempSkill);
+        this.tempSkill = "";
+      }
+    },
+  },
+};
 </script>
 
 <style>
+form {
+  max-width: 420px;
+  margin: 30px auto;
+  background: white;
+  text-align: left;
+  padding: 40px;
+  border-radius: 10px;
+}
 
-    form{
-        max-width: 420px;
-        margin: 30px auto;
-        background: white;
-        text-align: left;
-        padding: 40px;
-        border-radius: 10px;
-    }
+label {
+  color: #aaa;
+  display: inline-block;
+  margin: 25px 0 15px;
+  font-size: 0.6cm;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  font-weight: bold;
+}
 
-    label{
-        color: #aaa;
-        display: inline-block;
-        margin: 25px 0 15px;
-        font-size: 0.6cm;
-        text-transform: uppercase;
-        letter-spacing: 1px;
-        font-weight: bold;
-    }
+input,
+select {
+  display: block;
+  padding: 10px 6px;
+  width: 100%;
+  box-sizing: border-box;
+  border: none;
+  border-bottom: 1px solid #ddd;
+  color: #555;
+}
 
-    input{
-        display: block;
-        padding: 10px 6px;
-        width: 100%;
-        box-sizing: border-box;
-        border: none;
-        border-bottom: 1px solid #ddd;
-        color: #555; 
-    
-    }
+input[type="checkbox"] {
+  display: inline-block;
+  width: 16px;
+  margin: 0 10px 0 0;
+  position: relative;
+  top: 2px;
+}
+
+#changeFontSize {
+  font-size: 10px;
+}
 </style>
